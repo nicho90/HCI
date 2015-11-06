4f1766da207b05e9c0133c96c595d10fccf98fe1
Christoph Kisfeld <spam@b-web.org>
2015-11-06 09:05:11 +0100

    Cookies

---
+++ public/js/setcookie.js
@@ -0,0 +1,11 @@
+var uniqid = Cookies.get('uniqid');
+if(typeof(uniqid) !== 'undefined'){
+	uniqid = Cookies.get('uniqid');
+	//$('p#userid').text('Welcome back, '+uniqid);
+} else {
+	var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+	uniqid = randLetter + Date.now();
+	Cookies.set('uniqid', uniqid, { expires: 1 });
+	//$('p#userid').text('Welcome, '+uniqid);
+}
+$('input#userid').attr('value', uniqid);
